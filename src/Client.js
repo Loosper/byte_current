f7cbe0c79803fe1024a445fdf9eb15e47f089a9f
Loosper <boian4o1@gmail.com>
2018-01-26 17:19:15 +0200

    Basic code structure

---
+++ src/Client.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './Client.css';
+
+class Client extends Component {
+    render() {
+        return (
+            <div className="Client">
+                <header className="Client-header">
+                    <h1 className="Client-title">Torrent app</h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Client;
